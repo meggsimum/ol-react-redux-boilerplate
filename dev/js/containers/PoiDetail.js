df36bea3b5a02b40a37839ad43501702a0d3737d
Christian Mayer <chris@meggsimum.de>
2017-08-07 14:56:16 +0200

    Rework PoiDetail class
    Refactors the PoiDetail class, so it is shown as a floating component
    over the map. Under the hood it uses an material-ui component to
    achieve this.

--- dev/js/containers/PoiDetail.js
+++ dev/js/containers/PoiDetail.js
@@ -1,25 +1,47 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import Paper from 'material-ui/Paper';
+import {List, ListItem} from 'material-ui/List';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import Divider from 'material-ui/Divider';
+
 /*
  * We need "if(!this.props.poi)" because we set state to null by default
  * */
 
 class PoiDetail extends Component {
     render() {
-        var style = {
-            height: '100px'
+        const style = {
+            height: 400,
+            width: 300,
+            margin: 20,
+            padding: 10,
+            textAlign: 'center',
+            display: 'inline-block',
+            position: 'absolute',
+            right: 20,
+            top: 60,
+            zIndex: 100000
         };
         if (!this.props.poi) {
             return (
-                <div style={style}>
-                    <h2>Kein POI ausgewählt...</h2>
+                <div>
+                  <Paper style={style} zDepth={3}>
+                      <h3>Kein POI ausgewählt...</h3>
+                  </Paper>
                 </div>
             );
         }
         return (
-            <div style={style}>
-                <h2>{this.props.poi.get('name')}</h2>
+            <div>
+              <Paper style={style} zDepth={3}>
+                  <h3>{this.props.poi.get('name')}</h3>
+              </Paper>
             </div>
         );
     }
