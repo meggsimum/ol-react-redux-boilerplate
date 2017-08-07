9ae2030cd9a714011edbc8bbee9c55a483dcbc6e
Christian Mayer <chris@meggsimum.de>
2017-08-07 14:54:16 +0200

    Add 'material-ui' package
    Adds 'material-ui' package for material-design support and reusable
    standard components.

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -5,31 +5,40 @@ import OlLayerOsm from '../containers/OlLayerOsm';
 import OlLayerWms from '../containers/OlLayerWms';
 import OlLayerVector from '../containers/OlLayerVector';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 require('../../scss/style.scss');
 
 const App = () => (
-    <div>
-        <h2>Awesome OpenLayers React WebGIS</h2>
-        <OlMap zoom="14" center={[925492, 6348178]} >
 
-            <OlLayerWms
-                url="https://maps.service24.rlp.de/gisserver/services/RP/RP_ETRS_Gt/MapServer/WmsServer"
-                layers="0"
-                tiled={true}
-            >
-            </OlLayerWms>
+    <MuiThemeProvider>
 
-            <OlLayerVector
-                format="GeoJSON"
-                featureProjection="EPSG:3857"
-                featureCollKey = "featCollShops"
-                selectable={true}
-            >
-            </OlLayerVector>
+        <div>
+            <h2>Awesome OpenLayers React WebGIS</h2>
+            <OlMap zoom="14" center={[925492, 6348178]} >
 
-        </OlMap>
-        <PoiDetail />
-    </div>
+                <PoiDetail />
+
+                <OlLayerWms
+                    url="https://maps.service24.rlp.de/gisserver/services/RP/RP_ETRS_Gt/MapServer/WmsServer"
+                    layers="0"
+                    tiled={true}
+                >
+                </OlLayerWms>
+
+                <OlLayerVector
+                    format="GeoJSON"
+                    featureProjection="EPSG:3857"
+                    featureCollKey = "featCollShops"
+                    selectable={true}
+                >
+                </OlLayerVector>
+
+            </OlMap>
+
+        </div>
+
+    </MuiThemeProvider>
 );
 
 export default App;
